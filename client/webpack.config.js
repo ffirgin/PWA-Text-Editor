5599f126c4b904dcfa4389a8dea1aaef32e57c83
Griffin <ffirgn@gmail.com>
2023-01-20 21:46:47 -0800

    files added from testing

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -33,9 +33,8 @@ module.exports = () => {
         publicPath: '/',
         icons: [
           {
-            src: path.resolve('src/images/icon.png'),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
           }
         ],
       }),
